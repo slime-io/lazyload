ac4009bae7a761ba4c80e1c2037f0be51aeb1797
cywang1905 <cywang1905@163.com>
2021-10-22 15:20:07 +0800

    update global-pilot and slime-boot image to latest

--- test/e2e/values.go
+++ test/e2e/values.go
@@ -8,10 +8,10 @@ var (
 	slimebootName        = "slime-boot"
 	istiodLabelKey       = "istio.io/rev"
 	istiodLabelV         = "1-10-2"
-	slimebootTag          = "v0.2.3-a3f72fe"
+	slimebootTag          = "v0.2.3-5bf313f"
 	lazyloadTag           = "v0.2.6-d808438"
 	globalSidecarTag      = "1.7.0"
-	globalSidecarPilotTag = "globalPilot-7.0-v0.0.3-833f1bd5c1"
+	globalSidecarPilotTag = "globalPilot-7.0-v0.0.3-713c611962"
 )
 
 type TestResource struct {
