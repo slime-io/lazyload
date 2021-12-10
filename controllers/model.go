517ad7e84d59aba70b09f5fb7dbef12806c3dcf8
王晨宇 <wangchenyu07@corp.netease.com>
2021-12-10 14:53:20 +0800

    ServiceFence.Spec adds namespace and label selector matcher

--- controllers/model.go
+++ controllers/model.go
@@ -8,6 +8,7 @@ package controllers
 import (
 	"slime.io/slime/framework/model"
 	modmodel "slime.io/slime/modules/lazyload/model"
+	"sync"
 )
 
 var log = modmodel.ModuleLog.WithField(model.LogFieldKeyPkg, "controllers")
@@ -15,4 +16,19 @@ var log = modmodel.ModuleLog.WithField(model.LogFieldKeyPkg, "controllers")
 type Diff struct {
 	Deleted []string
 	Added   []string
+}
+
+type LabelItem struct {
+	Name  string
+	Value string
+}
+
+type NsSvcCache struct {
+	Data map[string]map[string]struct{}
+	sync.RWMutex
+}
+
+type LabelSvcCache struct {
+	Data map[LabelItem]map[string]struct{}
+	sync.RWMutex
 }
